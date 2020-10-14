a53285c290565592dbd0f1bab2abd72442ef5678
Gerard Brull <gerard.brull@zopa.com>
2020-10-14 10:59:36 +0200

    Fix scroll

--- src/components/scroll-arrow.js
+++ src/components/scroll-arrow.js
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
 
 const ScrollArrow = () => {
   const [showScroll, setShowScroll] = useState(false);
+  useEffect(() => {
+    const checkScrollTop = () => {
+      if (!showScroll && window && window.pageYOffset > 400) {
+        setShowScroll(true);
+      } else if (showScroll && window && window.pageYOffset <= 400) {
+        setShowScroll(false);
+      }
+    };
 
-  const checkScrollTop = () => {
-    if (!showScroll && window.pageYOffset > 400) {
-      setShowScroll(true);
-    } else if (showScroll && window.pageYOffset <= 400) {
-      setShowScroll(false);
-    }
-  };
+    window.addEventListener("scroll", checkScrollTop);
+  }, []);
 
   const scrollTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window && window.scrollTo({ top: 0, behavior: "smooth" });
   };
-
-  window.addEventListener("scroll", checkScrollTop);
 
   return (
     <span
