6b67ccbf00f763fc087075ebb1f5ba40e7c5fdee
Gerard Brull <gerard.brull@zopa.com>
2021-01-09 15:56:51 +0100

    Fix react-spring

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+// Fix for the graph (react-spring) https://github.com/pmndrs/react-spring/issues/1069
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  if (stage.startsWith("build-javascript")) {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-spring/,
+            sideEffects: true
+          }
+        ]
+      }
+    })
+  }
+}
