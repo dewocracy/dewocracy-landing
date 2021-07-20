179faa1afe94cc6a2825abc86bddbcc11a44fcb3
Philippe Naegeli <phn@anyweb.ch>
2021-07-20 17:08:06 +0200

    Add Cookiebot

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,22 @@
 import PropTypes from "prop-types";
 import React from "react";
-
+import CookieBot from "react-cookiebot";
 import Header from "./header";
 import { Footer } from "./footer";
 import ScrollArrow from "./scroll-arrow";
 
 function Layout({ children }) {
+
+  const cookieBotId = process.env.COOKIEBOT_GROUP_ID
+
   return (
     <div className="flex flex-col min-h-screen font-sans text-gray-900">
       <ScrollArrow />
       <Header />
       <main className="overflow-x-hidden">{children}</main>
       <Footer />
+      <CookieBot domainGroupId={cookieBotId} />
+
     </div>
   );
 }
