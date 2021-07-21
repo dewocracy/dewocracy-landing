77349e061913148ae713e329df2c653f86b86571
Philippe Naegeli <phn@anyweb.ch>
2021-07-21 04:26:03 +0200

    Move Cookiebot

--- src/components/header.js
+++ src/components/header.js
@@ -10,18 +10,24 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import dwBlue from "./../images/dewocracy-blue.svg";
 import { Dropdown } from "./dropdown";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import CookieBot from "react-cookiebot";
+
 
 function Header() {
   const [isExpanded, toggleExpansion] = React.useState(false);
   const { languages, language, originalPath } = useI18next();
   const { t } = useTranslation();
   const handleClickSubmenu = useCallback(() => toggleExpansion(!isExpanded));
+  const domainGroupId = process.env.COOKIEBOT_GROUP_ID
+
   return (
     <header
       id="header"
       className="grid grid-cols-3 md:flex items-center justify-center md:justify-between content-start pl-0 md:px-8 py-4
        md:py-6 w-screen bg-white h-18 sticky top-0 z-50 transition"
     >
+      {typeof window !== 'undefined' && <CookieBot domainGroupId={domainGroupId} />}
+
       <div className="pl-4">
         <button
           aria-label="menu"
@@ -37,9 +43,8 @@ function Header() {
         </button>
         <nav
           id="nav-menu-ex-1"
-          className={`${
-            isExpanded ? `block` : `hidden`
-          } md:items-center w-full md:w-auto text-primary`}
+          className={`${isExpanded ? `block` : `hidden`
+            } md:items-center w-full md:w-auto text-primary`}
         >
           <a
             className="block pb-2 pt-4 hover:underline"
@@ -56,9 +61,9 @@ function Header() {
             <Trans>Pricing</Trans>
           </a>
           <a
-              className="block py-2 hover:underline"
-              href="https://app.dewocracy.com/"
-              onClick={handleClickSubmenu}
+            className="block py-2 hover:underline"
+            href="https://app.dewocracy.com/"
+            onClick={handleClickSubmenu}
           >
             <Trans>Access</Trans>
           </a>
@@ -103,9 +108,8 @@ function Header() {
         aria-label="Go to home"
       >
         <figure
-          className={`flex items-center no-underline ${
-            isExpanded ? `hidden` : `block`
-          }`}
+          className={`flex items-center no-underline ${isExpanded ? `hidden` : `block`
+            }`}
         >
           <img width="150px" height="60px" src={dwBlue} alt="DeWocracy logo" />
         </figure>
@@ -118,8 +122,8 @@ function Header() {
           <Trans>Demo</Trans>
         </a>
         <a
-            className="block py-2 hover:underline"
-            href="https://app.dewocracy.com/"
+          className="block py-2 hover:underline"
+          href="https://app.dewocracy.com/"
         >
           <Trans>Access</Trans>
         </a>
@@ -149,9 +153,8 @@ function Header() {
             <Link
               key={key}
               to={originalPath}
-              className={`block p-4 pl-6 hover:underline ${
-                lang === language ? "underline" : ""
-              }`}
+              className={`block p-4 pl-6 hover:underline ${lang === language ? "underline" : ""
+                }`}
               role="menuitem"
               language={lang}
             >
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,12 @@
 import PropTypes from "prop-types";
 import React from "react";
-import CookieBot from "react-cookiebot";
 import Header from "./header";
 import { Footer } from "./footer";
 import ScrollArrow from "./scroll-arrow";
 
+
 function Layout({ children }) {
 
-  const cookieBotId = process.env.COOKIEBOT_GROUP_ID
 
   return (
     <div className="flex flex-col min-h-screen font-sans text-gray-900">
@@ -15,7 +14,6 @@ function Layout({ children }) {
       <Header />
       <main className="overflow-x-hidden">{children}</main>
       <Footer />
-      <CookieBot domainGroupId={cookieBotId} />
 
     </div>
   );
