a4b4db14da35759b553d26ec69cb871b03427240
Philippe Naegeli <phn@anyweb.ch>
2021-09-26 01:36:32 +0200

    Add Metadata

--- components/seo.js
+++ components/seo.js
@@ -1,5 +1,18 @@
 import PropTypes from "prop-types";
 import React from "react";
+
+/*
+const siteMetadata = {
+  "siteUrl": "",
+  title: `DeWocracy`,
+  description: `DeWocracy offers remote work solutions for a hybrid office strategy. Manage your company's human capital, control the office occupancy rate, lower costs and increase flexibility without compromising productivity.`,
+  author: `@de_wocracy`,
+  linkedin: "https://www.linkedin.com/company/dewocracy/",
+  twitter: "https://twitter.com/de_wocracy",
+  medium: "https://medium.com/dewocracy",
+  image: "src/images/banner-dw.png",
+}*/
+
 function SEO() {
 
 
