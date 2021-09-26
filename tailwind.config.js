5a77fe7b23bfbea2a72b5a2e64d3438f61c29c02
Philippe Naegeli <phn@anyweb.ch>
2021-09-26 02:06:50 +0200

    Update TailwindCSS stuff and dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-// See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  purge: ["./src/**/*.js"],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     container: {
       padding: {
