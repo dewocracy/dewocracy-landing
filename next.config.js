34086737232bb0763b95bcdefbfec597fff8570d
Philippe Naegeli <phn@anyweb.ch>
2021-09-29 12:02:11 +0200

    Improve Build times

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const nextBuildId = require('next-build-id')
+
+
 module.exports = {
     i18n: {
         locales: ['en', 'es', 'ca'],
         defaultLocale: 'es',
-    }
+    },
+    generateBuildId: () => nextBuildId({ dir: __dirname })
+
 }
 
