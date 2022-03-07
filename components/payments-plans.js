41ecfd339e7dca6b38a7ce5bcf3e07fc96652634
phn <pichichi91@users.noreply.github.com>
2022-03-07 17:18:02 -0500

    feat: update pricing section (#26)
    Co-authored-by: Philippe Naegeli <phn@anyweb.ch>

--- components/payments-plans.js
+++ components/payments-plans.js
@@ -1,12 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { useTranslations } from 'next-intl';
 import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
 import { OutboundLink } from "../utils/OutboundLink";
+import Toggle from 'react-toggle'
+import { set } from "lodash";
 
 
 export const PaymentsPlans = () => {
   const t = useTranslations("payment_plans");
+
+  const [isMonthly, setIsMonthly] = useState(true);
 
   return (
     <div className="relative z-10">
@@ -22,6 +26,18 @@ export const PaymentsPlans = () => {
         {t('title_question')}
       </h2>
 
+
+      <div className="ml-20 flex justify-center" >
+          <label className=" switch md:max-w-md">
+            <Toggle className=" form-check-input"
+              defaultChecked={true}
+              icons={false}
+              onChange={() => setIsMonthly(!isMonthly)} />
+            <span className="slider round"></span>
+
+          </label>
+          <div className="ml-4 pt-2 w-36">{t(isMonthly ? 'monthly_cost' : 'yearly_cost')} </div>
+        </div>
       <div className="grid lg:grid-cols-2 gap-10 justify-center h-full">
         <div
           data-sal="zoom-in"
@@ -36,20 +52,12 @@ export const PaymentsPlans = () => {
               <span className="font-bold">0 €</span>
               <span className="text-sm">
                 {" "}
-                {t('per_month')}
+                {t(isMonthly ? 'per_month' : 'per_year')}
               </span>
             </p>
           </div>
           <ol className="text-black grid gap-6">
 
-            <li className="flex items-center">
-              <FontAwesomeIcon
-                icon={faCheckCircle}
-                size="lg"
-                className="text-primary mr-4 text-primary-400"
-              />
-              {t('unlimited_users')}
-            </li>
             <li className="flex items-center">
               <FontAwesomeIcon
                 icon={faCheckCircle}
@@ -104,28 +112,22 @@ export const PaymentsPlans = () => {
         <div
           data-sal="zoom-in"
           data-sal-easing="ease"
-          className=" grid rounded-lg bg-white p-8 md:p-10 max-w-sm gap-y-8 my-4 lg:my-16"
+          className="grid rounded-lg bg-white p-8 md:p-10 max-w-sm gap-y-8 my-4 lg:my-16"
         >
           <div>
             <p className="text-primary text-2xl md:text-3xl font-bold  pb-4">
               {t('business_plan_title')}
             </p>
-            <OutboundLink
-              eventProperties={{ location: "payment-plans-box" }}
-              className=" mt-4 text-sm   border-b-2 pb-1 hover:border-black text-primary-400 hover:text-primary-800 leading-8 "
-              href="//calendly.com/dewocracylabs/dewocracy-demo" >
-              {t('talk_with_an_expert')}
-            </OutboundLink>
+            <p className="text-primary text-2xl md:text-3xl">
+              <span className="font-bold">{isMonthly ? 3 : 24} €</span>
+              <span className="text-sm">
+                {" "}
+                {t(isMonthly ? 'per_month' : 'per_year')}
+              </span>
+            </p>
+
           </div>
           <ol className="text-black grid gap-6">
-            <li className="flex items-center">
-              <FontAwesomeIcon
-                icon={faCheckCircle}
-                size="lg"
-                className="text-primary mr-4 text-primary-400"
-              />
-              {t('unlimited_users')}
-            </li>
             <li className="flex items-center">
               <FontAwesomeIcon
                 icon={faCheckCircle}
